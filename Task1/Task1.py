449f0249b5cec647205fd485aca61bb463fee6a0
VadimKutovoi <sir.kutovoi@yandex.ru>
2018-10-18 18:08:30 +0300

    added frequent words

--- Task1/Task1.py
+++ Task1/Task1.py
@@ -1,7 +1,23 @@
-def count_of_patterns(pattern, genome)
-	res = int()
-	for i in range(len(genome) - len(pattern)):
-    if pattern == genome[i:i + len(pattern)]:
-        res += 1
+def count_of_patterns(pattern, genome):
+    res = int()
+    for i in range(len(genome) - len(pattern)):
+        if pattern == genome[i:i + len(pattern)]:
+            res += 1
+    return res
+
+def frequent_words(genome, k):
+    patterns = {}
+    frequent_patterns = []
+    for i in range(len(genome) - k):
+        if genome[i: i + k] not in patterns:
+            patterns.update({genome[i: i + k]: count_of_patterns(genome[i: i + k], genome)})
+    maxcount = max(patterns.values())
+    for key, value in patterns.items():
+        if value == maxcount:
+            frequent_patterns.append(key)
+    return frequent_patterns
 
 
+
+print(count_of_patterns('ATAT','GATATATGCATATACTT'))
+print(frequent_words('ACGTTGCATGTCGCATGATGCATGAGAGCT', 4))
